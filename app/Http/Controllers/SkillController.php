5c1284c5a6e4171f502cc23724a8259134af77c5
unknown <muh.saggy22@gmail.com>
2022-11-06 10:32:59 +0700

    modify edit and delete button

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\SkillResource;
 use Inertia\Inertia;
 use App\Models\Skill;
 use Illuminate\Http\Request;
+use App\Http\Resources\SkillResource;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Redirect;
 use Intervention\Image\ImageManagerStatic as Image;
 
@@ -84,9 +85,9 @@ class SkillController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Skill $skill)
     {
-        //
+        return Inertia::render('Skills/Edit', compact('skill'));
     }
 
     /**
@@ -96,9 +97,23 @@ class SkillController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Skill $skill)
     {
-        //
+        $image = $skill->image;
+        $request->validate([
+            'name'=> ['required', 'min:3']
+        ]);
+        if ($request->hasFile('image')) {
+            Storage::delete($skill->image);
+            $image = $request->file('image')->store('skills');
+        }
+
+        $skill->update([
+            'name' => $request->name,
+            'image' => $image,
+        ]);
+
+        return Redirect::route('skills.index');
     }
 
     /**
@@ -107,8 +122,11 @@ class SkillController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Skill $skill)
     {
-        //
+        Storage::delete($skill->image);
+        $skill->delete();
+
+        return Redirect::back();
     }
 }
--- resources/js/Pages/Skills/Index.vue
+++ resources/js/Pages/Skills/Index.vue
@@ -34,9 +34,9 @@
                                     <img :src="skill.image" class="w-12 h-12 rounded-full">
                                 </td>
                                 <td class="py-4 px-6">
-                                    <button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Edit</button>
-
-                                    <button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                                    <Link :href="route('skills.edit',skill.id)"  class="font-medium text-blue-500 hover:text-blue-700 mr-2">Edit</Link>
+                                    <Link :href="route('skills.destroy',skill.id)" 
+                                    method="delete" as="button" type="button" class="font-medium text-red-500 hover:text-red-700 mr-2">Delete</Link>
                                 </td>
                             </tr>
                         </tbody>
