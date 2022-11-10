9b87d4b506679f73996f4da0c2de050986623e0b
unknown <muh.saggy22@gmail.com>
2022-11-10 10:26:36 +0700

    added header and footer

---
+++ resources/js/Components/Frontend/Footer.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+<template>
+    <footer class="bg-light-secondary dark:bg-dark-secondary py-4">
+        <div class="container mx-auto">
+            <div class="flex flex-col md:flex-row space-y-6 lg:space-y-0 items-center justify-between">
+                <div class="flex space-x-6 items-center justify-center">
+                    <img class="h-8 w-auto" src="https://flowbite.com/docs/images/logo.svg" alt="Logo">
+                </div>
+                <p class="text-paragraph opacity-80 text-[15px]">
+                    &copy; 2022 Muhamad Saggy All right reserved
+                </p>
+            </div>
+        </div>
+    </footer>
+</template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,14 +1,19 @@
 <script setup>
 import { Head, Link } from '@inertiajs/inertia-vue3';
+import FrontendLayout from '@/Layouts/Frontend.vue';
 
-defineProps({
-    canLogin: Boolean,
-    canRegister: Boolean,
-    laravelVersion: String,
-    phpVersion: String,
-})
+
 </script>
 
 <template>
-<Head title="Welcome to Portfolio" />
+    <Head title="Welcome to Portfolio" />
+    <FrontendLayout>
+        <!-- Hero primary -->
+        <!-- Promote tail-100 -->
+        <!-- About Secondary -->
+        <!-- Skills tail-100 -->
+        <!-- Portfolio Primary -->
+        <!-- Services secondary -->
+        <!-- Contact primary -->
+    </FrontendLayout>
 </template>
